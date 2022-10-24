2f45378bfe022f13cee7030737cdce620bcffe53
iSymSmith <61099081+iSymSmith@users.noreply.github.com>
2022-10-24 16:49:36 -0500

    Wacky Tile Push

--- Assets/Scripts/GridManager.cs
+++ Assets/Scripts/GridManager.cs
@@ -4,29 +4,86 @@ using UnityEngine;
 using UnityEngine.Tilemaps;
 
 public class GridManager : MonoBehaviour
-
-
     //A placeholder script that will hold info about the grid, not currently being used
 {
-    public Grid grid;
-    private int[,] gridarray;
-    private int columns = 16;
-    private int rows = 16;
+    public GameObject waterTilePrefab;
+    public GameObject groundTilePrefab;
+    public Grid gridObject;
+    public Sprite[] sprites;
+    public int[,] grid;
+    private int columns = 49;
+    private int rows = 49;
 
     private Tile[] tileArray;
-
-    public Tilemap groundMap;
     // Start is called before the first frame update
     void Start()
     {
+        grid = new int[columns, rows];
+        for(int i = 0; i < columns; i ++){
+            for(int j = 0; j < rows; j ++){
+                if(i >= 20 && j >= 20 && i <= 39 && j <= 39){
+                    grid[i,j] = 1;
+                } else{
+                    grid[i,j] = 0;
+                }
+                
+            } 
+        }
+        for(int i = 0; i < columns; i ++){
+            for(int j = 0; j < rows; j ++){
+                if(i > 0 && j > 0 && i < (rows - 1) && j < (columns - 1) && grid[i,j] > 0){
+                    grid[i,j] = IsEdge(i,j);
+                } 
+                spawnTile(i, j);
+            } 
+        }
+    }
 
+    // Update is called once per frame
+    private void spawnTile(int x, int y)
+    {
+        SpriteRenderer sr = new SpriteRenderer();
+        if(grid[x,y] > 0){
+            sr = Instantiate(groundTilePrefab, new Vector3(x+0.5f, y+0.5f), Quaternion.identity).GetComponent<SpriteRenderer>();
+            sr.name = ("X: " + x + "Y: " + y);
+            sr.sprite = sprites[grid[x,y]];
+        } else{
+            sr = Instantiate(waterTilePrefab, new Vector3(x+0.5f, y+0.5f), Quaternion.identity).GetComponent<SpriteRenderer>();
+            sr.name = ("X: " + x + "Y: " + y);
+        }
         
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private int IsEdge(int x, int y){
+        Debug.Log(grid[x + 1,y] == 0 && grid[x,y+1] > 0 && grid[x-1, y] > 0 && grid[x,y-1] > 0);
+        if(grid[x + 1,y] > 0 && grid[x,y+1] > 0 && grid[x-1, y] > 0 && grid[x,y-1] > 0 && grid[x + 1,y + 1] > 0 && grid[x - 1,y - 1] > 0 && grid[x-1, y+1] > 0 && grid[x + 1,y-1] > 0){
+            return 1;
+        } else if(grid[x + 1,y] > 0 && grid[x,y+1] > 0 && grid[x-1, y] > 0 && grid[x,y-1] > 0 && grid[x + 1,y + 1] > 0 && grid[x - 1,y - 1] > 0 && grid[x-1, y+1] == 0 && grid[x + 1,y-1] > 0){
+            return 2;
+        } else if(grid[x + 1,y] > 0 && grid[x,y+1] == 0 && grid[x-1, y] > 0 && grid[x,y-1] > 0){
+            return 3;
+        } else if(grid[x + 1,y] > 0 && grid[x,y+1] > 0 && grid[x-1, y] > 0 && grid[x,y-1] > 0 && grid[x + 1,y + 1] == 0 && grid[x - 1,y - 1] > 0 && grid[x-1, y+1] > 0 && grid[x + 1,y-1] > 0){
+            return 4;
+        } else if(grid[x + 1,y] == 0 && grid[x,y+1] > 0 && grid[x-1, y] > 0 && grid[x,y-1] > 0){
+            return 5;
+        } else if(grid[x + 1,y] > 0 && grid[x,y+1] > 0 && grid[x-1, y] > 0 && grid[x,y-1] > 0 && grid[x + 1,y + 1] > 0 && grid[x - 1,y - 1] > 0 && grid[x-1, y+1] > 0 && grid[x + 1,y-1] == 0){
+            return 6;
+        } else if(grid[x + 1,y] > 0 && grid[x,y+1] > 0 && grid[x-1, y] > 0 && grid[x,y-1] == 0){
+            return 7;
+        } else if(grid[x + 1,y] > 0 && grid[x,y+1] > 0 && grid[x-1, y] > 0 && grid[x,y-1] > 0 && grid[x + 1,y + 1] > 0 && grid[x - 1,y - 1] == 0 && grid[x-1, y+1] > 0 && grid[x + 1,y-1] > 0){
+            return 8;
+        } else if(grid[x + 1,y] > 0 && grid[x,y+1] > 0 && grid[x-1, y] == 0 && grid[x,y-1] > 0){
+            return 9;
+        } else if(grid[x + 1,y] > 0 && grid[x,y+1] == 0 && grid[x-1, y] == 0 && grid[x,y-1] > 0 && grid[x-1, y+1] == 0 && grid[x + 1,y-1] > 0){
+            return 10;
+        } else if(grid[x + 1,y] == 0 && grid[x,y+1] == 0 && grid[x-1, y] > 0 && grid[x,y-1] > 0 && grid[x + 1,y + 1] == 0 && grid[x - 1,y - 1] > 0){
+            return 11;
+        } else if(grid[x + 1,y] == 0 && grid[x,y+1] > 0 && grid[x-1, y] > 0 && grid[x,y-1] == 0 && grid[x-1, y+1] > 0 && grid[x + 1,y-1] == 0){
+            return 12;
+        } else if(grid[x + 1,y] > 0 && grid[x,y+1] > 0 && grid[x-1, y] == 0 && grid[x,y-1] == 0 && grid[x + 1,y + 1] > 0 && grid[x - 1,y - 1] == 0){
+            return 13;
+        }
+    return 1;
     }
 }
